44cef6c92557b1602db6cce3c326e820d9f7bd46
ryder <ryderjt13@gmail.com>
2026-01-09 12:14:16 -0700

    Initial Commit

---
+++ Type Bot/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Type Bot
+//
+//  Created by Ryder Thomas on 1/9/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Type Bot/Type_BotApp.swift
@@ -0,0 +1,17 @@
+//
+//  Type_BotApp.swift
+//  Type Bot
+//
+//  Created by Ryder Thomas on 1/9/26.
+//
+
+import SwiftUI
+
+@main
+struct Type_BotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
